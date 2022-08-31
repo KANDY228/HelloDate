c21bb3bb76de1495c349ad1b4a14a8d20fb0a890
KANDY228 <71720867+KANDY228@users.noreply.github.com>
2022-08-31 13:50:35 +0300

    Philosophy of Java task ( 2 ) -HelloDate
    HelloDate.Java

---
+++ src/test/java/HelloDate/java/REll/AppTest.java
@@ -0,0 +1,20 @@
+package HelloDate.java.REll;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
